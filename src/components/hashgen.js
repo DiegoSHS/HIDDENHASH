4596037e235f5d00b07132476a270186d5a692b9
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2023-10-10 11:18:57 -0600

    added description
    Signed-off-by: Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>

--- src/components/hashgen.js
+++ src/components/hashgen.js
@@ -1,5 +1,10 @@
 import { createHash } from "crypto"
-
+/**
+ * Cifra un texto con un algoritmo hash
+ * @param {String} text texto a convertir
+ * @param {String} method algoritmo de cifrado
+ * @returns hash con formato String
+ */
 export const hasher = (text, method = 'sha256') => {
     return createHash(method).update(text).digest("hex")
 }
