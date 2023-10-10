ee300fac27c049c13b1e191e2eaf19482a997251
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2023-10-10 10:48:45 -0600

    tittle modified
    Signed-off-by: Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,7 @@ export default function Home({ algorithms }) {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Generador de hash</title>
         <meta name="description" content="Hash app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
