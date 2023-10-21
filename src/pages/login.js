00a0e0b47d66b6b8defd589708a166974b798490
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2023-10-20 18:44:40 -0600

    modified behavior in sesions (#26)
    Signed-off-by: Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>

--- src/pages/login.js
+++ src/pages/login.js
@@ -29,7 +29,7 @@ const Login = () => {
                 <Button
                     onClick={() => {
                         signOut()
-                        push('/')
+                        router.push('/')
                     }}
                     fullWidth
                     variant="contained"
