968de1c0c3c850222196b447a20ba6e70316d3fc
Diego López Ariza <diegoawesome2017@outlook.com>
2023-10-25 22:21:42 -0600

    updated id route
    Signed-off-by: Diego López Ariza <diegoawesome2017@outlook.com>

--- src/pages/api/sendfile/[id].js
+++ src/pages/api/sendfile/[id].js
@@ -1,8 +1,9 @@
 import { IncomingForm } from "formidable"
 import { readFile, unlink, writeFile } from "fs/promises"
+
 const formidableParse = async (req) =>
     new Promise((resolve, reject) =>
-        new IncomingForm().parse(req, (err, files) => err ? reject(err) : resolve([files]))
+        new IncomingForm().parse(req, (err, fields, files) => err ? reject(err) : resolve([fields, files]))
     )
 
 export const config = {
@@ -11,20 +12,20 @@ export const config = {
     }
 }
 
-async function readAndWriteFile(singleImg, newPath, query) {
+async function readAndWriteFile({ originalFilename, filepath }, newPath, query) {
     try {
-        const data = await readFile(singleImg.filepath)
-        const fullpath = `${newPath}/${singleImg.originalFilename}`
-        await writeFile(fullpath, data)
+        const path = `${newPath}/${originalFilename}`
+        const data = await readFile(filepath)
+        await writeFile(path, data)
         const formData = new FormData()
-        formData.append('file', new Blob([data], { type: 'appicaiton/octet-stream' }), singleImg.originalFilename)
+        formData.append('file', new Blob([data], { type: 'appicaiton/octet-stream' }), originalFilename)
         formData.append('type', JSON.stringify({ len: query[0], dig: query[1], lw: query[2], up: query[3], sp: query[4], ct: query[5] }))
-        const send = await fetch(`${process.env.PYTHON_API}/bruteforce/`, {
+        const send = await fetch(`http://127.0.0.1:8000/bruteforce/`, {
             method: 'POST',
             body: formData
         })
         const pass = await send.json()
-        await unlink(fullpath)
+        await unlink(path)
         return pass
     } catch (error) {
         console.log(error.message)
@@ -33,15 +34,16 @@ async function readAndWriteFile(singleImg, newPath, query) {
 
 export default async function (req, res) {
     const { query: { id } } = req
-    const [files] = await formidableParse(req)
-    const allFiles = files.files
-    const passwords = allFiles.map((file) => readAndWriteFile(file, './public/uploads', id.split('-')))
+    const [_, { files }] = await formidableParse(req)
+    const passwords = files.map(async file => readAndWriteFile(file, './public/uploads', id.split('-')))
     const results = await Promise.allSettled(passwords)
-    const values = results.map(e => e.value)
+    const values = results.map(({ value }) => value)
     if (values.length === 0) {
         return res.status(200).json({ error: { code: 200, message: 'Intentalo de nuevo' } })
     }
-    if (results.every(e => e.status === 'fulfilled')) {
+    if (results.every(({ status }) => status === 'fulfilled')) {
         return res.status(200).json(values[0])
+    } else {
+        return res.status(408).json({ error: { code: 408, message: 'Tardó demasiado la respuesta' } })
     }
 }
