919a21ea84706c31c9f3f41f06fc8c71877e87c4
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2023-10-25 23:54:07 -0600

    updated cors (#64) (#65) (#66)
    Signed-off-by: Diego López Ariza <diegoawesome2017@outlook.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-Requested-With, Accept, Content-Length,Content-Type" },
+        ],
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
