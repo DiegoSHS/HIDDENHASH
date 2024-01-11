8c9b751c338b0b1cee38147d9ff71239a92b3335
Diego López Ariza <diegoawesome2017@outlook.com>
2024-01-10 22:29:07 -0600

    Minos changes in pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import { Apple, Duo, GitHub, Google } from '@mui/icons-material'
-import { Box, Button, IconButton, Typography } from '@mui/material'
+import { Apple, Duo, GitHub, Google, Twitter, WhatsApp, YouTube } from '@mui/icons-material'
+import { Box, Button, Card, CardContent, IconButton, Typography } from '@mui/material'
+import { blue, green, grey, red } from '@mui/material/colors'
 import Link from "next/link"
 
 export default function Home() {
@@ -30,23 +31,23 @@ export default function Home() {
         </Link>
       </Box>
       <Box>
-        <Typography variant='h4' fontWeight={600} align='center' letterSpacing={2} sx={{ mt: 5 }}>
-          Sponsors (no en verdad)
+        <Typography variant='h4' fontWeight={600} align='center' letterSpacing={2} sx={{ my: 2 }}>
+          Sponsors
         </Typography>
         <Box alignItems={'center'} display={'flex'} alignContent={'center'}>
           <Link legacyBehavior href={'https://google.com/'} passHref>
             <IconButton>
-              <Google></Google>
+              <Google htmlColor={blue[300]}></Google>
             </IconButton>
           </Link>
           <Link href={'https://apple.com/'} passHref legacyBehavior>
             <IconButton>
-              <Apple></Apple>
+              <Apple htmlColor={grey[600]}></Apple>
             </IconButton>
           </Link>
           <Link href={'https://duo.google.com/'} passHref legacyBehavior>
             <IconButton>
-              <Duo></Duo>
+              <Duo htmlColor={blue[400]}></Duo>
             </IconButton>
           </Link>
           <Link href={'https://github.com/'} passHref legacyBehavior>
@@ -54,8 +55,62 @@ export default function Home() {
               <GitHub></GitHub>
             </IconButton>
           </Link>
+          <Link href={'https://youtube.com/'} passHref legacyBehavior>
+            <IconButton>
+              <YouTube htmlColor={red[700]}></YouTube>
+            </IconButton>
+          </Link>
+          <Link href={'https://whatsapp.com/'} passHref legacyBehavior>
+            <IconButton>
+              <WhatsApp htmlColor={green[400]}></WhatsApp>
+            </IconButton>
+          </Link>
+          <Link href={'https://twitter.com/'} passHref legacyBehavior>
+            <IconButton>
+              <Twitter htmlColor={blue[200]}></Twitter>
+            </IconButton>
+          </Link>
         </Box>
       </Box>
-    </Box>
+      <Box>
+        <Typography variant='h4' fontWeight={600} align='center' letterSpacing={2} sx={{ my: 2 }}>
+          Opiniones
+        </Typography>
+        <Box display={'flex'} flexDirection={'row'} alignContent={'center'} alignItems={'center'}>
+          <Card variant='outlined' sx={{ backgroundColor: '#00000050', m: 1 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Diego
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Desarrollador
+              </Typography>
+              <Typography variant="body2">
+                El proyecto es genial y sigue mejorando.
+                <Typography color={'orange'}>
+                  "Increible de inicio a fin"
+                </Typography>
+              </Typography>
+            </CardContent>
+          </Card>
+          <Card variant='outlined' sx={{ backgroundColor: '#00000050', m: 1 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Asunción
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Desarrollador
+              </Typography>
+              <Typography variant="body2">
+                Todo bien pero falta el algoritmo sm3.
+                <Typography color={'orange'}>
+                  "Todos los algoritmos"
+                </Typography>
+              </Typography>
+            </CardContent>
+          </Card>
+        </Box>
+      </Box>
+    </Box >
   )
 }
